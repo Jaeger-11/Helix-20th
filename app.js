ba0efb70597962066f60e2b4ada77875aa38c1ea
Falodun Damilola <dpaulzs11@gmail.com>
2023-04-27 23:10:45 +0100

    parking fumctionalities

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const parks = document.querySelector('#parks');
 const freeplaces = document.querySelector('#freeplaces');
 const walletvalue = document.querySelector('#walletvalue');
+const currentpage = document.querySelector('#currentpage');
+const lastpage = document.querySelector('#lastpage');
 
 const parkingData = [
     {
@@ -53,10 +55,180 @@ const parkingData = [
         cost: '1.540',
         status: 'free',
     },
+    {
+        id: '131',
+        cost: '1.540',
+        status: 'occupied',
+    },
+    {
+        id: '132',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '133',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '134',
+        cost: '1.540',
+        status: 'occupied',
+    },
+    {
+        id: '135',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '136',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '137',
+        cost: '1.540',
+        status: 'occupied',
+    },
+    {
+        id: '138',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '139',
+        cost: '1.540',
+        status: 'selected',
+    },
+    {
+        id: '140',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '141',
+        cost: '1.540',
+        status: 'occupied',
+    },
+    {
+        id: '142',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '143',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '144',
+        cost: '1.540',
+        status: 'occupied',
+    },
+    {
+        id: '145',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '146',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '147',
+        cost: '1.540',
+        status: 'occupied',
+    },
+    {
+        id: '148',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '149',
+        cost: '1.540',
+        status: 'selected',
+    },
+    {
+        id: '150',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '151',
+        cost: '1.540',
+        status: 'occupied',
+    },
+    {
+        id: '152',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '153',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '154',
+        cost: '1.540',
+        status: 'occupied',
+    },
+    {
+        id: '155',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '156',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '157',
+        cost: '1.540',
+        status: 'occupied',
+    },
+    {
+        id: '158',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '159',
+        cost: '1.540',
+        status: 'selected',
+    }
+    ,
+    {
+        id: '160',
+        cost: '1.540',
+        status: 'occupied',
+    },
+    {
+        id: '161',
+        cost: '1.540',
+        status: 'free',
+    },
+    {
+        id: '162',
+        cost: '1.540',
+        status: 'selected',
+    }
 ]
 
+let max = 10;
+let min = 0;
+
 const pushParksData = ( data ) => {
-    data.map((item) => {
+    // PAGE FUNCTIONALITY
+    let maxpage = Math.ceil( parkingData.length / 10 )
+    let presentpage = Math.ceil(max / 10)
+    parks.innerHTML = "";
+    lastpage.textContent = maxpage;
+    currentpage.textContent = presentpage;
+    data.slice(min, max).map((item) => {
         const { id, cost, status } = item;
         // THE STYLING HERE RELIES DIRECTLY ON THE STATUS IN THE DATA
 
@@ -91,15 +263,31 @@ document.addEventListener('keydown', evt => {
 
 document.addEventListener('keydown', evt => {
     if (evt.key === 'q') {
-        alert('Q pressed');
+        min -= 10;
+        max -= 10;
+        if ( min < 0 ){
+            min += 10
+            max += 10
+            pushParksData(parkingData);
+        }
+        pushParksData(parkingData);
     }
 });
 
 document.addEventListener('keydown', evt => {
     if (evt.key === 'e') {
-        alert('E pressed');
+        min += 10 
+        max += 10
+        if ( min >= parkingData.length ){
+            min -= 10
+            max -= 10
+            pushParksData(parkingData);
+        }
+        pushParksData(parkingData);
     }
 });
+
+console.log(min, max)
 
 pushParksData(parkingData);
 setWalletValue('130.000');
